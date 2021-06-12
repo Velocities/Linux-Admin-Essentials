ce0f901f7f8d81b45c86b2c40e2dfeacea6a4a2e
Velocities <84917738+Velocities@users.noreply.github.com>
2021-06-12 16:50:50 -0400

    Add files via upload

--- navig.sh
+++ navig.sh
@@ -4,7 +4,9 @@ TODAY=`date`
 DIR=`pwd`
 DISTRO=`grep -w NAME /etc/os-release | cut -d= -f2 | cut -d\" -f2`
 VERSION=`grep -w VERSION /etc/os-release | cut -d= -f2 | cut -d\" -f2`
+GRPS=`groups $USERNAME | cut -d: -f2`
 echo "Hello $USERNAME"
+echo "You are in the following groups: $GRPS"
 echo "Your current working directory is $DIR"
 echo "You are on $DISTRO version $VERSION"
 echo "The date is $TODAY"
